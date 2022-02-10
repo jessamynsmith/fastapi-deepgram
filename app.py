0395fd7a783b56539908d97077ff11467b738176
jessamynsmith <geekchick77@gmail.com>
2022-02-09 22:03:41 -0700

    Added deepgram integration

--- app.py
+++ app.py
@@ -1,5 +1,56 @@
+import os
 import socketio
 import uvicorn
+from deepgram import Deepgram
+
+
+DEEPGRAM_API_KEY = os.environ.get('DEEPGRAM_API_KEY')
+
+dg_socket = None
+
+
+async def transcript_handler(data):
+    print('transcript handler')
+    if 'channel' in data:
+        transcript = data['channel']['alternatives'][0]['transcript']
+        print('received transcript:', transcript)
+        await sio.emit('message', f"received: {transcript}")
+        print("sent")
+
+
+async def connect_to_deepgram():
+    global dg_socket
+
+    if dg_socket:
+        print('Socket to deepgram is already open')
+        return
+
+    # Initialize the Deepgram SDK
+    dg_client = Deepgram(DEEPGRAM_API_KEY)
+
+    # Create a websocket connection to Deepgram
+    try:
+        dg_socket = await dg_client.transcription.live({'punctuate': True})
+
+        # Listen for the connection to close
+        dg_socket.registerHandler(dg_socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
+
+        # Print incoming transcription objects
+        dg_socket.registerHandler(dg_socket.event.TRANSCRIPT_RECEIVED, transcript_handler)
+    except Exception as e:
+        print(f'Could not open socket: {e}')
+
+
+async def process_audio(connection, data):
+    print('processing audio')
+    await sio.emit('message', f"received audio")
+
+    connection.send(data)
+
+    # Indicate that we've finished sending data
+    print('finishing')
+    await connection.finish()
+    print('finished')
 
 
 static_files = {
@@ -14,13 +65,14 @@ sio = socketio.AsyncServer(async_mode='asgi')
 @sio.on('message')
 async def print_message(sid, message):
     print("Socket ID: ", sid)
-    print(message)
     await sio.emit('message', f"received: {message}")
 
 
 @sio.on('audio')
-async def print_message(sid, message):
+async def print_message(sid, data):
     print("Socket ID: ", sid)
+    await connect_to_deepgram()
+    await process_audio(dg_socket, data)
 
 
 # wrap with ASGI application
